e49123f23ae51cb385c71246d61e20655d030553
Aung Myo Kyaw <aungmyokyaw.git@icloud.com>
2021-12-26 15:45:59 +0630

    chore: update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-console.log("www.aungmyokyaw.com")
+console.log("www.aungmyokyaw.com");
+var amkElement = document.getElementById("amk");
+// amk.style.height = window.screen.height + "px";
+// amk.style.width = window.screen.width + "px";
+amk.style.height = window.innerHeight + "px";
+amk.style.width = window.innerWidth + "px";
