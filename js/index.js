6f089099cf83159e1506a053f755014bd221640e
Aung Myo Kyaw <aungmyokyaw.git@icloud.com>
2021-12-26 21:32:53 +0630

    feat: change bg every 8 sec

--- js/index.js
+++ js/index.js
@@ -1,7 +1,43 @@
 console.log("www.aungmyokyaw.com");
-var amkElement = document.getElementById("amk");
-var overlay = document.getElementById("overlay");
-amk.style.height = window.innerHeight + "px";
-amk.style.width = window.innerWidth + "px";
-overlay.style.height = window.innerHeight + "px";
-overlay.style.width = window.innerWidth + "px";
+var gBackgroundImg = backgroundImgUrlGen();
+function setWidthAndHeight() {
+  var amkElement = document.getElementById("amk");
+  var overlay = document.getElementById("overlay");
+  amkElement.style.height = window.innerHeight + "px";
+  amkElement.style.width = window.innerWidth + "px";
+  overlay.style.height = window.innerHeight + "px";
+  overlay.style.width = window.innerWidth + "px";
+}
+
+function backgroundImgUrlGen() {
+  var backgroundImgWidth = window.innerWidth;
+  var backgroundImgHeight = window.innerHeight;
+  var backgroundImgUrl =
+    "https://picsum.photos/" +
+    backgroundImgWidth +
+    "/" +
+    backgroundImgHeight +
+    ".jpg?random=" +
+    Math.floor(Math.random() * 1e3);
+  return backgroundImgUrl;
+}
+
+function setBackground(setNow) {
+  if (!setNow) {
+    gBackgroundImg = backgroundImgUrlGen();
+    checkBackgroundImageIsLoaded(gBackgroundImg);
+    return false;
+  }
+  var amkElement = document.getElementById("amk");
+  amkElement.style.backgroundImage = "url(" + gBackgroundImg + ")";
+}
+function checkBackgroundImageIsLoaded(srcUrl) {
+  var backgroundImageElement = document.getElementById("backgroundImageLoader");
+  backgroundImageElement.src = srcUrl;
+  backgroundImageElement.onload = function () {
+    setBackground(backgroundImageElement.complete);
+  };
+}
+setWidthAndHeight();
+setBackground(backgroundImgUrlGen(), false);
+window.setInterval(setBackground, 8000, false);
