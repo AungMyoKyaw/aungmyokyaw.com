4a95becdf5dac977e5e1384670f459e1a11b9ff7
Aung Myo Kyaw <aungmyokyaw.git@icloud.com>
2022-07-02 19:44:25 +0630

    feat: svelte

---
+++ svelte/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  import Background from './lib/Background.svelte'
+  import Message from './lib/Message.svelte'
+</script>
+
+<main>
+  <Background />
+  <Message />
+</main>
+
+<style>
+</style>
